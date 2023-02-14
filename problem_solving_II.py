2123fed2a4a917154a6fbdf4a300c4ed50640146
Ethan Doering <ethanwdoering@icloud.com>
2023-02-14 13:14:12 -0700

    Added the capitalize_first_letter function

--- problem_solving_II.py
+++ problem_solving_II.py
@@ -1,17 +1,32 @@
 #Task 1: Reverse a String
 def reverse_string():
     reversed_string = ""
-    user_string = input("Please enter in a string for me to reverse: ")
+    user_string = input("\n\nPlease enter in a string for me to reverse: ")
 
     for index in range(len(user_string) -1, -1, -1):
         reverse_string += user_string[index]
 
     return reverse_string
 
-
-
 #Task 2: Capitalize a Letter
+def capitalize_first_letter():
+    capitalized_string = ""
+    user_string = input("\n\nPlease enter in a multiword string and I will capitalize the first letter of every word: ")
+
+    for index in range(len(user_string)):
+        if index == 0 and user_string[index] != " ":
+            capitalized_string += str(user_string[0].upper())
+        elif user_string[index-1] == " ":
+            capitalized_string += str(user_string[index].upper())
+        else:
+            capitalized_string += str(user_string[index])
+
+    return capitalized_string
 
 #Task 3:  Palindrome
 
 #Task 4(bonus): Compress a string of characters
+capitalized_string = ""
+
+capitalized_string = capitalize_first_letter()
+print(capitalized_string)
