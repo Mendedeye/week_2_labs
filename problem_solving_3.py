340111d509fd1b6e740ad2c463dbd706647d91c6
Ethan Doering <ethanwdoering@icloud.com>
2023-02-15 10:12:51 -0700

    Debugged and and got it into a running order

--- problem_solving_3.py
+++ problem_solving_3.py
@@ -3,16 +3,16 @@
 #Sums the square of the digits in a number
 def is_happy_number():
     is_happy = True
-    number = 0
     user_input = input("\n\nPlease enter in a number to determine if it is a happy number or not: ")
+    number = user_input
     happy_list = []
 
     while is_happy == True and number != 1:
-        number = digit_invariant(user_input)
+        number = digit_invariant(number)
         if list_checker(number, happy_list):
             is_happy = False
         else:
-            list.append(number)
+            happy_list.append(number)
 
     return user_input, is_happy
 
@@ -31,8 +31,17 @@ def list_checker (number, list):
 
 def digit_invariant(number):
     total = 0
-    for digit in number:
+    for digit in str(number):
         total += pow(int(digit),2)
 
     return total
 
+def run_happy_numbers():
+    user_input, is_happy = is_happy_number()
+
+    if is_happy == True:
+        print(f"{user_input} is a happy number!!!")
+    else:
+        print(f"{user_input} is not a happy number. :(")
+
+run_happy_numbers()
